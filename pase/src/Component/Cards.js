a8c49bd0c6b97a2a376afd88ec12f056bbb8a00d
oppong yeboah <oppongyeboahjnr@gmail.com>
2020-08-11 16:59:07 +0000

    Create Cards.js

---
+++ pase/src/Component/Cards.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+class Cards extends Component {
+    render(){
+        return(    
+        <div className="container">
+          <div className="row">
+            <div  className="card2 col-md-3 mb-3">
+            <div className="card-body">
+                <h4>Total Cases
+                </h4><p>120,00,000</p> 
+            </div>
+            </div>
+
+             <div  className="card2 col-md-3 mb-3">
+            <div className="card-body">
+                <h4>Total Cases
+                </h4><p>120,00,000</p> 
+            </div>
+            </div>
+
+             <div  className="card2 col-md-3 mb-3">
+            <div className="card-body">
+                <h4>Total Cases
+                </h4><p>120,00,000</p> 
+            </div>
+            </div>
+
+             <div  className="card2 col-md-3 mb-3">
+            <div className="card-body">
+                <h4>Total Cases
+                </h4><p>120,00,000</p> 
+            </div>
+            </div>
+
+            </div>
+         </div>
+          
+           
+         
+           
+            
+           
+        
+        )
+    }
+    
+}
+export default Cards
